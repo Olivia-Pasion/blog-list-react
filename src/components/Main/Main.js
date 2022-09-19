223b10a8a12c2a4a48d9222e3c5dc06d09587f36
olivia pasion <oap9820@gmail.com>
2022-09-19 13:06:51 -0700

    working on Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,2 +1,22 @@
 import BlogCard from '../BlogCard/BlogCard.js';
-import
+import { useBlogs } from '../../hooks/useBlogs';
+
+export default function Main() {
+  const { filterBlogs, title, setTitle } = useBlogs();
+
+  return {
+    <Main>
+      <select
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      >
+      <option value ="all">All</option>
+      </select>
+      {filterBlogs().map((blog) => (
+        <BlogCard key={blog.title} {...blog} />
+      ))}
+    </Main>
+  }
+}
